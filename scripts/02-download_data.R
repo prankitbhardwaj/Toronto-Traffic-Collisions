bb169793ff6b305dc508dbed28c76ce8d41793d6
prankitbhardwaj <prankit.bhardwaj@mail.utoronto.ca>
2024-12-02 19:43:03 -0500

    Download data script done for two datasets

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Opendata Toronto
+# Author: Prankit Bhardwaj
+# Date: 28 November 2024
+# Contact: prankit.bhardwaj@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -11,16 +11,55 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+#Get neighbourhood package
+neighbour_package <- show_package("neighbourhoods")
 
+#List all neighbourhood package resources
+neighbourhood_resources <- list_package_resources("neighbourhoods")
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#Identify the neighbourhood resources into csv and GeoJSON
+neighbourhood_data <- filter(neighbourhood_resources, tolower(format) %in% c('csv', 'geojson'))
+
+#Download each file and save them into the raw data folder
+for (i in seq_len(nrow(neighbourhood_data))) {
+  resource <- neighbourhood_data[i, ]
+  file_format <- tolower(resource$format)
+  data <- get_resource(resource)
+  
+  if (file_format == "csv") {
+    write.csv(data, "data/raw_data/neighbourhoods.csv", row.names = FALSE)
+  } else if (file_format == "geojson") {
+    writeLines(jsonlite::toJSON(data, pretty = TRUE), "data/raw_data/neighbourhoods.geojson")
+  }
+}
+
+########### Downloading collisions data ###########
+
+#Get trafic collisions package
+collisions_package <- show_package("ec53f7b2-769b-4914-91fe-a37ee27a90b3")
+
+#List all neighbourhood package resources
+collisions_resources <- list_package_resources("ec53f7b2-769b-4914-91fe-a37ee27a90b3")
+
+#Identify the neighbourhood resources into csv and GeoJSON
+collisions_data <- filter(collisions_resources, tolower(format) %in% c('csv', 'geojson'))
+
+#Download each file and save them into the raw data folder
+#NOTE: Files are extreme
+for (i in seq_len(nrow(collisions_data))) {
+  resource <- collisions_data[i, ]
+  file_format <- tolower(resource$format)
+  data <- get_resource(resource)
+  
+  if (file_format == "csv") {
+    write.csv(data, "data/raw_data/collisions.csv", row.names = FALSE)
+  } else if (file_format == "geojson") {
+    writeLines(jsonlite::toJSON(data, pretty = TRUE), "data/raw_data/collisions.geojson")
+  }
+}
 
          
